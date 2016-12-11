4a79640eb3e18a0ab532871120976e29eae88dc6
eieiiii <eikun.kawai@gmail.com>
2016-12-11 15:12:30 +0900

    update

--- studyapp/app/src/main/java/kawaieisuke/com/studyapp/MainActivity.java
+++ studyapp/app/src/main/java/kawaieisuke/com/studyapp/MainActivity.java
@@ -2,12 +2,24 @@ package kawaieisuke.com.studyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int count=0;
+    TextView countTextview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countTextview = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void plus(View view){
+        count = count+1;
+        countTextview.setText(String.valueOf(count));
     }
 }
